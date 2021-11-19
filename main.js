b78fdd316a4d2267449497a88e872939201c12b2
Davina Leong <davina.leong.sy@gmail.com>
2021-11-20 05:47:15 +0800

    Able to split names

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    //
+    const randomiseNamesInput = $("#randomiseNames textarea[name='names']");
+    const randomiseNamesSubmit = $("#randomiseNames .submit");
+
+    randomiseNamesSubmit.click(function() {
+        const names = splitVal(randomiseNamesInput);
+        console.log(names);
+    });
 });
+
+function splitVal(element) {
+    return element.val().split(/\r?\n/);
+}
