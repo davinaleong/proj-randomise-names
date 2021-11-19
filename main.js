35a8cd7a17e5db68f1fbc877cb46846cd90b6ce3
Davina Leong <davina.leong.sy@gmail.com>
2021-11-20 06:02:40 +0800

    Render values from input

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
     const randomiseNamesInput = $("#randomiseNames textarea[name='names']");
     const randomiseNamesSubmit = $("#randomiseNames .submit");
+    const randomiseNamesList = $("#randomiseNames .list");
+    const randomiseNamesAssigned = $("#randomiseNames .assigned");
 
     randomiseNamesSubmit.click(function() {
         const names = splitVal(randomiseNamesInput);
-        console.log(names);
-    });
-});
+        const namesCopy = names.slice();
 
-function splitVal(element) {
-    return element.val().split(/\r?\n/);
-}
+        clearRandomiseNames();
+        randomiseNamesList.html(generateListHtml(namesCopy));
+        // TODO: randomise names
+    });
+
+    function splitVal(element) {
+        return element.val().split(/\r?\n/);
+    }
+
+    function generateListHtml(array) {
+        let olHtml = '<ol>';
+        array.map(item => {
+            olHtml += `<li>${item}</li>`;
+        });
+        olHtml += '</ol>';
+
+        return olHtml;
+    }
+
+    function clearRandomiseNames() {
+        randomiseNamesInput.val('');
+        randomiseNamesList.html('');
+        randomiseNamesAssigned.html('');
+    }
+});
